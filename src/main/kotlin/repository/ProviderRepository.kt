e8e1f3e481b90f3e08fdb2799492b31885bf2e9d
aaracan246 <acalberto@iesrafaelalberti.es>
2024-10-30 20:09:15 +0100

    avanzando en métodos de productv4

--- src/main/kotlin/repository/ProviderRepository.kt
+++ src/main/kotlin/repository/ProviderRepository.kt
@@ -1,8 +1,16 @@
 package org.example.repository
 
+import org.example.utils.HibernateUtils
+import java.security.Provider
+
 class ProviderRepository {
 
-    fun getProvidersProd(){}
+    private val em = HibernateUtils.getEntityManagerFactory()
+
+    fun getProvidersProd(): List<Provider>{
+        val query = em.createQuery("SELECT provider FROM providers ", Provider::class.java) // !!!!!???
+        return query.resultList
+    }
 
     fun getAllProviders(){}
 }
